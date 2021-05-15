70aa98847cc1d076f02e8823d882b8cd987334f5
Bruno Calderon <slardaromg@gmail.com>
2021-05-15 19:47:27 -0400

    updating script.js

--- script.js
+++ script.js
@@ -1,114 +1,75 @@
 
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",'Z'];
+var simbols =["@","#","$","%","&","(",")","=","+","{","}","!"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
-//characters for password
-
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",'Z']
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-var simbols =["@","#","$","%","&","(",")","=","+","{","}","!"]
-
-//input variables
-
-
-var simbolsC;
-var numberC;
-var lowercaseC;
-var uppercaseC;;
-var LoC; //length of characters
-
+//Initial question and popping alert for userInput 
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
-function generatepassword() {
-  var LoC = window.prompt("Add number of characters") ;
-  
-  //if no number added then//
-  if (!LoC) {
-    window.alert("please add the required information");
-  }
-  
-  //limit of characters 8-128//
-  else if (LoC <8 || LoC >128){
-    LoC = window.alert("the limit is between 8 and 128");
-  }
-  else{
-    numberC = window.confirm("Add numbers?");
-    simbolsC = window.confirm("Add special characters?")
-    lowercaseC = window.confirm("Add lowercase characters?");
-    uppercaseC = window.confirm("Add uppercase character?");
-  }
-  var passwordcharacters = [];
-//if not confirmed any option//
-  if (!numberC && !uppercaseC && !lowercaseC && !simbolsC) {
-    userchoices = window.alert("please add the required information");}
-//if all options are confirmed
-    else if (numberC && uppercaseC && lowercaseC&& simbolsC) {
+function generatePassword() {
+    var usernumber = prompt("Select a number between 8 to 128")
 
-      passwordcharacters =  numbers.concat(uppercase,lowercase,simbols);
-   }
-// if 3 options are confirmed
-    else if (numberC && uppercaseC && lowercaseC){
-      passwordcharacters = numbers.concat(uppercase,lowercase);
-    }
-    else if (numberC && uppercaseC && simbolsC){
-      passwordcharacters = numbers.concat(uppercase,simbols);
-    }
-    else if (numberC && lowercaseC && simbolsC) {
-      passwordcharacters = numbers.concat(lowercase,simbols);
-    }
-    else if (uppercaseC && lowercaseC && simbolsC){
-      upasswordcharacters = uppercase.concat(lowercase,simbols);
-    }
-  // if 2 options are confirmed 
 
-    else if (numberC &&uppercaseC){
-      passwordcharacters = numbers.concat(uppercase);
+    //limit of numbers
+
+    if (usernumber < 8 || usernumber > 128 ) {
+        alert("only numbers between 8 to 128")
+        return
     }
-    else if (numberC && lowercaseC){
-      passwordcharacters = numbers.concat(lowercase);
+    // confirm for details of password
+    var chooselowers = confirm("Add lowercase letters?");
+    var chooseuppers = confirm("Add uppercase letters?");
+    var choosesimbols = confirm("Add special characters?");
+    var choosenumbers = confirm("Add numbers ?");
+
+    var selected = []
+    var finalpassword = ""
+//conditionals for characters
+    if (chooselowers === true) {
+      selected = selected.concat(lowerCase)
     }
-    else if (numberC && simbolsC){
-      passwordcharacters = numbers.concat(simbolsC);
+
+    if (chooseuppers === true) {
+      selected = selected.concat(upperCase)
     }
-    else if (simbolsC && lowercaseC){
-      passwordcharacters =  simbols.concat(lowercase);
+    if (choosesimbols === true) {
+
+      selected = selected.concat(simbols)
+
     }
-    else if (simbolsC && uppercaseC){
-      passwordcharacters = simbols.concat(uppercase);
+    if (choosenumbers === true) {
+      selected = selected.concat(numbers)
     }
-    else if (lowercaseC && uppercaseC){
-      passwordcharacters  = lowercase.concat(uppercase);
+    
+    // random characaters
+
+
+    for (var i = 0; i < usernumber; i++) {
+        var mixed = selected[
+            Math.floor(Math.random() * selected.length)];
+
+        finalpassword = mixed + finalpassword
+
     }
-// if 1 option is confirmed
-    else if (numberC) {
-      passwordcharacters = numbers;
+    console.log(finalpassword)
+
+
+    return finalpassword
 }
-    else if (simbolsC){
-      passwordcharacters = simbols;
-    }
-    else if (lowercaseC){
-      passwordcharacters = lowercase;
-    }
-    else if (uppercaseC){
-      passwordcharacters = uppercase;
-    }
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-    //random password
-    var password = []
+// Write password to the #password input
+function writePassword() {
+    //     generatePassword()
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-
-  for (var i = 0; i < LoC; i++){
-    password = password + passwordcharacters [Math.floor(Math.random()*passwordcharacters.length)];
-    window.alert(password)}
-    return randomPassword
-}
-  function writePassword() {
-    var password = generatepassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
+generateBtn.addEventListener("click", writePassword);
